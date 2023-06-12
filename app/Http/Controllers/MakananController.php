f12583c0dc7093d89f5e21760dbc677963733d1a
Afif Uddin <afifuddin@Afifs-MacBook-Air.local>
2023-06-12 09:39:18 +0700

    [UPDATE] Last Commit

--- app/Http/Controllers/MakananController.php
+++ app/Http/Controllers/MakananController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Makanan;
 use Illuminate\Http\Request;
 
 class MakananController extends Controller
 {
     public function tampil()
     {
-        $makanans = "Bakso";
+        $makanans = Makanan::all();
+
+        //cek database makanan
+        // dd($makanans);
 
         return view('welcome', ['makanans' => $makanans]);
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
 
-<body>
-    <h1>Haloo Dunia {{ $makanans }} </h1>
+<body class="p-5">
+    <h1>Daftar Makanan</h1>
+
+    {{-- <ul>
+        @foreach ($makanans as $makanan)
+            <img src={{ $makanan->gambar }} width="100" />
+
+            <li>{{ $makanan->nama }} - Rp. {{ number_format($makanan->harga) }}</li>
+        @endforeach
+    </ul> --}}
+    <div class="flex gap-5">
+        @foreach ($makanans as $makanan)
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img class="w-full h-64" src="{{ $makanan->gambar }}" alt="Sunset in the mountains">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">{{ $makanan->nama }}</div>
+                    <p class="text-gray-700 text-base">Rp. {{ number_format($makanan->harga) }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
 </body>
 
 </html>
